7aa58e9e816ee332eaa07e0f4cbbae3943ba95ba
SwiftCode <SwiftCode@gamil.com>
2020-07-03 09:15:43 +0800

    Initial Commit

---
+++ SNSShareSDK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SNSShareSDK
+//
+//  Created by SwiftiCode on 3/7/20.
+//  Copyright © 2020 SwiftiCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
